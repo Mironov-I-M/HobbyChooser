19c33be4f350f9e93458c797ecd38f0a5ddccb48
Mironov <takeda.mihailovich@gmail.com>
2025-11-01 21:51:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        compare(1);
+        compare(2);
+        compare(5);
+        compare(15);
+    }
+
+    public static void compare(int day) {
+        System.out.println("=== Day " + day + " ===");
+        int[] startNumbers = {21, 1, 20, 23};
+        int iterative = chooseHobbyIterative(startNumbers, day);
+        int recursive = chooseHobbyRecursive(startNumbers, day, new int[day]);
+        System.out.println("Iterative = " + iterative + " | Recursive = " + recursive);
+        System.out.println();
+    }
+
+    public static int chooseHobbyRecursive(int[] startNumbers, int day, int[] memory) {
+        int[] mixedArray = new int[startNumbers.length + memory.length];
+
+        int prv = mixedArray[mixedArray.length - 1 - day];
+        int prePrePrev = mixedArray[mixedArray.length - 1 - day - 2];
+        int result = (prv * prePrePrev) % 10 + 1;
+
+        if (day == 1) {
+            return result;
+        }
+
+        memory[memory.length - day] = result;
+
+        return chooseHobbyRecursive(startNumbers, day - 1, memory);
+    }
+
+    public static int chooseHobbyIterative(int[] startNumbers, int day) {
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(startNumbers[0]);
+        numbers.add(startNumbers[1]);
+        numbers.add(startNumbers[2]);
+        numbers.add(startNumbers[3]);
+
+        for (int d = 0; d < day; d++) {
+            int index = d + 4;
+            int prev = numbers.get(index - 1);
+            int prePrePrev = numbers.get(index - 3);
+            numbers.add((prev * prePrePrev) % 10 + 1);
+        }
+
+        return numbers.get(numbers.size() - 1);
+    }
+}
